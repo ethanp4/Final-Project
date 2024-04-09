37ae59561c070c99121aea587b2bb67b5296d5e5
Ethan <157554972+ethanp4@users.noreply.github.com>
2024-04-09 02:13:02 -0600

    log requests

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,10 @@ app.use(express.urlencoded({ extended: false }))
 // convert json to javascript object and put into request body
 app.use(express.json())
 app.use(allRoutes)
+app.use((req, res, next) => {
+  console.log(`Request: ${req.method} ${req.url}`)
+  next()
+})
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`)
 })
